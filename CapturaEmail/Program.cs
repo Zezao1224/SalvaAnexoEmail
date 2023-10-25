bcd867202c9268ae9501feb90d6b3d7a0f82a8f2
Zezao1224 <gamer12224@yahoo.com.br>
2023-10-25 00:19:17 -0300

    funcionando.

--- CapturaEmail/Program.cs
+++ CapturaEmail/Program.cs
@@ -1,55 +1,34 @@
-﻿using System.Net.Mail;
-using MailKit.Net.Pop3;
-using MailKit;
-using MimeKit;
+﻿using System.Net;
+using System.Net.Mail;
+using OpenPop.Mime;
+using OpenPop.Pop3;
 
-
-//Servidor de entrada de e-mails (POP)
-
-//Servidor: pop.mail.yahoo.com
-//Porta: 995
-//Requer SSL: SimServidor de entrada de e-mails (POP)
-
-//Servidor: pop.mail.yahoo.com
-//Porta: 995
-//Requer SSL: Sim
-Console.WriteLine("Hello, World!");
 using (Pop3Client client = new Pop3Client())
 {
-
-    client.Connect("pop.mail.yahoo.com", 995);
+    client.Connect("pop.mail.yahoo.com", 995, true);
+    client.Authenticate("user", "senha", AuthenticationMethod.UsernameAndPassword);
     int messageCount = client.GetMessageCount();
-    List<MimeMessage> allMessages = new List<MimeMessage>(messageCount);
-
-    for (int i = messageCount-1; i > 0; i--)
+    List<Message> allMessages = new List<Message>(messageCount);
+    //Verifica se os 10 últimos e-mails possuem anexo.
+    for (int i = messageCount - 10; i > 0; i--)
     {
-        if (client.GetMessage(i).Attachments.Any())
+        if (client.GetMessage(i).FindAllAttachments().Count > 0)
         {
             allMessages.Add(client.GetMessage(i));
-             Message message = client.GetMessage(i); 
-            MessagePart messagePart = message.MessagePart.MessageParts[1];
-            client.GetMessage(i).Body.ToString();
-            client.GetMessage(i).Attachments.FirstOrDefault();
         }
     }
-    foreach (MimeMessage msg in allMessages)
+    foreach (Message msg in allMessages)
     {
+        List<MessagePart> ats = msg.FindAllAttachments();
+        foreach (MessagePart at in ats)
+        {
+            string fileName = at.FileName;
+            string finalPath = Path.Combine("E:\\temp", fileName);
+            File.WriteAllBytes(finalPath, at.Body);
+        }
 
-        //var att = MimePart.Load(msg);
-        //att.save(new System.IO.FileInfo(System.IO.Path.Combine("c:\\xlsx", ado.FileName)));
     }
-    //for (int i = 0; i < client.Count; i++)
-    //{
-    //    MimeMessage message = client.GetMessage(i);
-    //    Console.WriteLine("Titulo: {0} ,Dt: {1}, anexo: {3}", message.Subject, 
-    //                                                          message.Date, 
-    //                                                          message.Attachments. );
-
-    //}
-
-    client.Disconnect(true);
+    client.Disconnect();
 }
 
 
-
-    Console.ReadLine();
